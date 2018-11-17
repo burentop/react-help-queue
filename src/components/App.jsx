8a42848658c8cf57bfb87a694d1815cf86654199
Brent Perry <burentoperii@gmail.com>
2018-11-17 15:30:41 -0600

    Break Ticket, TicketList out into components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(){
-  return ();
+  return (
+    <div>
+      <Header/>
+      <TicketList/>
+    </div>
+  );
 }
 
 export default App;
